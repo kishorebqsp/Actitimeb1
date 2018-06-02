09c5b7c7b9518a6d42d36d43d6444dc767f1baf1
bharani <kishoreqsp009@gmail.com>
2018-06-02 11:49:56 +0530

    hiiigm

---
+++ src/Generic/Basetest.java
@@ -0,0 +1,24 @@
+package Generic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Basetest implements Constant{
+	public WebDriver driver;
+	@BeforeMethod
+	public void openapp()
+	{
+		System.setProperty(GECKO_KEY,GECKO_VALUE);
+	    driver=new FirefoxDriver();
+		driver.get("http://localhost/login.do");
+	}
+	
+	@AfterMethod
+	public void closeapp()
+	{
+		driver.quit();
+	}
+
+}
